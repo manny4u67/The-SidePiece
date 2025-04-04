dfcc213e6ef21548f3024829ab239460eacdb058
manny4u67 <manny4u67@gmail.com>
2025-04-04 01:59:24 -0400

    Rotary Encoder Functional

--- Prototype Files/Firmware/sp_proto1d/keymaps/via/keymap.c
+++ Prototype Files/Firmware/sp_proto1d/keymaps/via/keymap.c
@@ -1,8 +1,8 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
-
+#include "quantum/encoder.h"
 #include QMK_KEYBOARD_H
-
+#define NUM_DIRECTIONS 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -26,3 +26,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_9,   KC_0
     )
 };
+
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) , ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) }
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_RIGHT);
+        } else {
+            tap_code(KC_LEFT);
+        }
+    }
+    return false;
+}
